58a65743530f50af11ad1a52bd17b33b804dee0a
Akshay Rawat <projects@akshay.cc>
2012-08-01 01:12:06 +0530

    silly bug fix

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -19,7 +19,7 @@ class WelcomeController < ApplicationController
   end
 
   def stale?
-    Delayed::Job.where('updated_at > ?',  1.hour.ago).present?
+    Delayed::Job.where('updated_at < ?',  1.hour.ago).present?
   end
 
 end
